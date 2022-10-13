222fd14cd3671595fba495fb287be00312d11da3
User -2022QJEKR\Administrator <qy15@naver.com>
2022-10-13 18:04:44 +0900

    gager

--- function/function.js
+++ function/function.js
@@ -1,104 +1,75 @@
 'use strict';
-{/* < 클로저 (Closuer) >
-    - 도달 가능한 외부변수에 접근이 가능한 함수.
-일반적으로 중첩함수에서 내부함수가 외부함수의 변수를 참조 가능한상태를 의미.js에서는 모든 함수가 클로저로 활용 가능. */}
 
-let outer1 = 5;
-// 전역변수는 전역 렉시컬환경객체(Global Lexcial Environment)의 프로퍼티.
+function testFunc() {
+    console.log(this);      // 일반함수에서 this는 객체를 할당 받기전 undefined 반환. 함수를 동적으로 객체에 할당하면
+}                           // 함수 내의 this는 동적 할당된 객체를 지정.
 
-function closure() {
-    console.log(outer1);
+// const testFunc = () => {
+//     console.log(this);
+// };
+
+const testArrow = () => {
+    console.log(this);      // 화살표 함수에서의 this는 내부적으로 익명 객체를 생성하므로 빈 객체를 반환.
+};
+
+testFunc();
+testArrow();
+
+
+
+const car1 = {
+    carName: '소나타',
+    viewCarName() {
+        function output() {
+            console.log(this.carName);  // viewCarName메서드 내에서 this는 할당 객체 car1의 carName을 가리키지만, output함수 내에서
+        }                               // this는 4행에서와 마찬가지로 ouput함수가 객체에 할당되지 않은 상태이므로 undefined를 반환
+        output();                       // 하여 this.carName은 컴파일 오류.
+    },
 }
 
-// 함수를 기준으로
-// 함수 내의 매개변수나 지역변수를 저장하는 내부 렉시컬 환경객체와
-// 함수 외부의 변수를 참조하는 경우의 외부 렉시컬 환경객체로 구분
-// 동작 방식은 먼저 내부 렉시컬 환경객체에서 변수를 검색 후 존재하지 않으면
-// 외부 렉시컬 환경객체에서 검색하는구조.
-// 따라서 지역변수와 전역변수명이 동일한 경우 지역변수가 참조되는 상황은 이러한 이유.
+// car1.viewCarName();
 
-closure();
+const car2 = {
 
-console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
-{/* <중첩함수 내에서의 외부변소 참조> */ }
+    carName: '아반떼',
+    // viewCarName: () => console.log(this.carName),    // 화살표 함수 내에서 this는 8행과 같이 빈 객체가 반환되므로 this.carName은
+    //                                                  // undefined 반환. 따라서 객체 내에 함수 정의 시 객체에 할당된 다른 프로퍼티를
+    //                                                  // 참조해야 하는 경우에는 람다식이 아닌 객체 프로퍼티에 익명함수를 할당하는
+    //                                                  // 방식을 사용. ( 17행과 같은 객체 메서드 축약 포함. )
+    //                                                  // 즉, 람다식은 객체의 다른 프로퍼티에 영향을 받지 않고 독립적인 기능을 하는
+    //                                                  // 동작의 전달을 해야하는 경우에 활용하는 것이 적합.
 
-function outerFunc() {
-    let outer2 = 10;
-
-    function innerFunc() {
-        console.log(outer2);
-        // 내부 함수인 innerFunc에 대한 내부 렉시컬 환경객체와 
-        // 외부 함수인 outerFunc1에 대한 외부 렉시컬 환경객체로 구분
-    }
-    innerFunc();
-}
-outerFunc();
-
-console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
-
-// 함수 내의 지역변수는 호출이 완료되면 소멸되지만, 도달 가능한 상태일 때까지는 그 값을 유지.
-// 즉, 함수의 호출이 끝나면 해당 함수에 대한 렉시컬 환경객체가 소멸되지만, 내부함수가 외부함수의
-// 렉시컬 환경객체(외부변수)를 참조하는 상태에서 반환된 내부함수의 참조를 유지하고 있는 동안은
-// 해당 외부 렉시컬 환경객체는 메모리에서 제거되지않고 유지되는 특성.
-function outerFunc2() {
-    let outer3 = 15;
-
-    // outerFunc2의 지연변수(outer3)는 호출이 완료되면 그 기억공간이 소멸되지만
-    // 내부함수에서 그 값을 참조하여 익명함수로 반환함으로써 그 참조를 통해 외부변수(outer3)에
-    // 도달 가능함으로써 outerFunc2 함수를 호출하여 반환받는 시점 까지도 외부변수(outer3) 기억공간은 유지
-
-    return function () {
-        console.log(outer3);
-    };
-
-}
-outerFunc2()();
-
-console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
-
-
-function outerFunc3() {
-    let outer4 = 20;
-
-    return function () {
-        return ++outer4;
-    };
+    viewCarName() {
+        const output = () => console.log(this.carName); // 메서드 내에서 화살표 함수의 this는 빈 객체를 참조하지 않고 자신을 포함하는
+        output();                                       // 메서드(viewCarName)의 this를 참조.
+    },
 }
 
-let innerFunc1 = outerFunc3();
+car2.viewCarName();
 
-console.log(innerFunc1());
-console.log(innerFunc1());
-console.log(innerFunc1());
+// const func = () => {
+//     console.log('전역 화살표 함수의 this >> ', this);
+// };
+// // 화살표 함수에는 this가 없습니다. 백날 불러도 안나옵니다.
 
-// outerFunc3 함수의 호출이 완료되었지만, 내부함수를 통해
-// 외부지역변수(outer4)에 도달 가능한 상태가 유지되므로
-// 외부지역변수(outer4)는 소멸되지 않고 메모리 유지.
+// func();
 
-console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+// console.log('');
 
-innerFunc1 = null;
-// innerFunc1 이 내부함수의 참조를 잃어버림으로써 기존의 렉시컬 환경객체가 소멸됨에 따라 지역변수 또한 소멸.
+// const Person = {
+//     name: 'NIHILncunia',
+//     getName1: () => {
+//         console.log('객체 내부에서의 화살표 함수의 this >> ', this);
+//     },
+//     getName2() {
+//         const name = () => {
+//             console.log('화살표 함수가 아닌 함수 안에서 화살표 함수의 this >> ', this);
+//         };
+//         name();
+//     }
+// };
 
-innerFunc1 = outerFunc3();
-//새로운 렉시컬 환경객체 생성
-console.log(innerFunc1());
 
-console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
-
-function outerFunc4() {
-    let outer5 = 25;
-
-    return function () {
-        return ++outer5;
-    };
-}
-
-//두 개의 독립적 렉시컬 환경객체 생성.
-
-let innerFunc2 = outerFunc4(),
-    innerFunc3 = outerFunc4();
-
-console.log(innerFunc2());
-console.log(innerFunc3());
-// 내부함수가 각각 개별적인 지역변수에 접근되는 것을 확인 가능.
+// console.log(Person.getName1());
+// console.log('');
+// console.log(Person.getName2());
